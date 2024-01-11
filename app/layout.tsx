fad4c55e06269dbc871dc69ec628b5ff51aefe07
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2024-01-11 10:29:20 -0500

    Deployment test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,51 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import React from 'react';
-import { Inter , Space_Grotesk } from 'next/font/google';
-import type { Metadata } from 'next';
-import './globals.css';
-import './favicon.ico';
-import { ThemeProvider } from '@/context/ThemeProvider';
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import "./favicon.ico";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 
 export const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500',
-  '600', '700', '800', '900'],
-  variable: '--font-inter'
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
 export const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500',
-  '600', '700'],
-  variable: '--spaceGrotesk',
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--spaceGrotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'BodhiOverflow',
-  description: 'Yes yes',
+  title: "BodhiOverflow",
+  description: "Yes yes",
   icons: {
-    icon: '/assets/images/site-logo.svg',
-  }
-}
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-   
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-                <ClerkProvider
-              appearance={{
-                elements: {
-                  formButtonPrimary: 'primary-gradient',
-                  footerActionLink: 'primary-text-gradient hover:text-primary-500'}
-              }}
-            >
-            <ThemeProvider>
-              {children}
-            </ThemeProvider> 
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
         </ClerkProvider>
-        </body>
- 
-      </html>
-   
-  )
+      </body>
+    </html>
+  );
 }
