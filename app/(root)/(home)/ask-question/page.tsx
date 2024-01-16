1951528257c3b7f69e050ea5f1d723f3e2aedaab
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2024-01-16 12:41:42 -0500

    fixed eslint errors

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,14 +1,13 @@
-import Question from '@/components/forms/Question'
-import { getUserById } from '@/lib/actions/user.action';
-import { auth } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
+import React from "react";
 
 const Page = async () => {
   // const { userId } = auth();
 
-  const userId = 'clerk123';
-  if (!userId) redirect('/sign-in');
+  const userId = "clerk123";
+  if (!userId) redirect("/sign-in");
 
   const MongoUser = await getUserById({ userId });
 
@@ -16,12 +15,12 @@ const Page = async () => {
 
   return (
     <div>
-      <h1 className='h1-bold text-dark400_light900'>Ask a Question</h1>
-        <div className='mt-9'>
-          <Question MongoUserId={JSON.stringify(MongoUser._id)}/>
-        </div>
+      <h1 className="h1-bold text-dark400_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question MongoUserId={JSON.stringify(MongoUser._id)} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- components/shared/leftsidebar/Leftsidebar.tsx
+++ components/shared/leftsidebar/Leftsidebar.tsx
@@ -1,95 +1,98 @@
-"use client"
-import { sidebarLinks } from '@/constants';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-import { SignedOut } from '@clerk/nextjs';
-
-
+"use client";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SignedOut } from "@clerk/nextjs";
 
 const Leftsidebar = () => {
   const pathname = usePathname();
 
   return (
-    <section className='justify-between background-light900_dark200 w-[266px]
-    min-h-screen top-0 sticky p-6 light-border left-0 flex flex-col overflow-y-auto
-    border-r pt-36 shadow-light-300 dark:shadow-none max-sm:hidden custom-scrollbar max-lg:w-[100px]'>
-        <div className='flex flex-1 flex-col gap-6'>
-            {sidebarLinks.map((item) => {
-                const isActive = (pathname.includes(item.route) && item.route.length > 1) || 
-                pathname === item.route;
+    <section
+      className="background-light900_dark200 light-border custom-scrollbar
+    sticky left-0 top-0 flex min-h-screen w-[266px] flex-col justify-between overflow-y-auto
+    border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-lg:w-[100px] max-sm:hidden"
+    >
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
 
-                return (
-                      <Link 
-                      key={item.route}
-                      href={item.route}
-                          className={`${isActive ? 'primary-gradient rounded-lg text-light-900': 'text-dark300_light900'}
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              }
                           flex items-center justify-start gap-4 bg-transparent p-4`}
-                      >
-                          <Image
-                          className={`${isActive ? "" : "invert-colors"}`}
-                              src={item.imgURL}
-                              alt={item.label}
-                              width={20}
-                              height={20}
-                          />
-                          <p className={`${isActive ? 'base-bold' : 'base-medium'} max-lg:hidden`}>{item.label}
-                          </p>
-                      </Link>
-                  )
-                
-            })}
+            >
+              <Image
+                className={`${isActive ? "" : "invert-colors"}`}
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+              />
+              <p
+                className={`${
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
+              >
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/accounts.svg"
+                alt="login"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log In
+              </span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                alt="signup"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Sign Up
+              </span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              Sign Up
+            </Button>
+          </Link>
         </div>
-
-        <SignedOut>
-  <div className='flex flex-col gap-3'>
-
-      <Link href="/sign-in">
-        <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
-          <Image 
-          src="/assets/icons/accounts.svg"
-          alt='login'
-          width={20}
-          height={20}
-          className='invert-colors lg:hidden'
-          />
-          <span className='primary-text-gradient max-lg:hidden'>
-            Log In
-          </span>
-        </Button>
-      </Link>
-
-      <Link href="/sign-up">
-        <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
-          <Image 
-          src="/assets/icons/sign-up.svg"
-          alt='signup'
-          width={20}
-          height={20}
-          className='invert-colors lg:hidden'
-          />
-          <span className='primary-text-gradient max-lg:hidden'>
-            Sign Up
-          </span>
-        </Button>
-      </Link>
-
-
-
-      <Link href="/sign-up">
-        <Button className='small-medium light-border-2 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none text-dark400_light900'>    
-          Sign Up
-        </Button>
-      </Link>
-
-  </div>
-</SignedOut>
+      </SignedOut>
     </section>
+  );
+};
 
-
-  )
-}
-
-export default Leftsidebar
+export default Leftsidebar;
