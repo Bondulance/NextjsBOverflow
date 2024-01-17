ab65a91cd34cbabcc517bc8aaae1726bba1e6052
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2024-01-17 18:09:27 -0500

    fixed route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,12 +1,12 @@
+/* eslint-disable camelcase */
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser, updateUser } from "@/lib/actions/user.action";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -32,7 +32,7 @@ export async function POST(req: Request) {
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
@@ -51,17 +51,13 @@ export async function POST(req: Request) {
     });
   }
 
-  // Get the ID and type
-  // const { id } = evt.type;
   const eventType = evt.type;
-
-  console.log({ eventType });
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // Create a new user in database
+    // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
       name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
@@ -71,11 +67,13 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ message: "OK", user: mongoUser });
-  } else if (eventType === "user.updated") {
+  }
+
+  if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // Create a new user in database
+    // Create a new user in your database
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
@@ -91,11 +89,14 @@ export async function POST(req: Request) {
   }
 
   if (eventType === "user.deleted") {
-    // const { id } = evt.data;
-    // const deletedUser = await deleteUser({
-    //   clerkId: id!,
-    // });
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser({
+      clerkId: id!,
+    });
+
+    return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return new Response("", { status: 200 });
+  return NextResponse.json({ message: "OK" });
 }
