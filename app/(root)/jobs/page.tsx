cbdbe64384b531f4a778762750bb03a3b0c360a4
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2024-02-23 17:22:14 -0500

    feature coming soon

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,17 @@
+import NoResult from "@/components/shared/NoResult/NoResult";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <NoResult
+        title="Feature coming soon!"
+        description="Thank you for you patience, for now go explore something else within BodhiOverflow!"
+        link="/"
+        linkTitle="Home"
+      ></NoResult>
+    </div>
+  );
+};
+
+export default page;
